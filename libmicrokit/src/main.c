a128972f4b0d8e67dfe99dd370a20d1e126365cc
zhewen shen <zhewen.shen@outlook.com>
2025-07-19 07:01:33 +1000

    cleanup

--- libmicrokit/src/main.c
+++ libmicrokit/src/main.c
@@ -32,6 +32,11 @@ seL4_Word microkit_irqs;
 seL4_Word microkit_notifications;
 seL4_Word microkit_pps;
 
+seL4_MessageInfo_t microkit_tag = {0};
+seL4_MessageInfo_t microkit_reply_tag = {0};
+
+static uintptr_t *pnk_mem = NULL;
+
 extern seL4_IPCBuffer __sel4_ipc_buffer_obj;
 
 seL4_IPCBuffer *__sel4_ipc_buffer = &__sel4_ipc_buffer_obj;
@@ -70,7 +75,7 @@ extern void *microkit_cml_stack;
 extern void *microkit_cml_stackend;
 extern void microkit_cml_main(void);
 
-static char microkit_cml_memory[1024*2];  // Reduced from 20KB to 2KB
+static char microkit_cml_memory[1024*2];
 
 void microkit_cml_exit(int arg) {
     microkit_dbg_puts("ERROR! We should not be getting here\n");
@@ -83,10 +88,6 @@ void microkit_cml_err(int arg) {
     microkit_cml_exit(arg);
 }
 
-/* Need to come up with a replacement for this clear cache function.
-    Might be worth testing just flushing the entire l1 cache,
-    but might cause issues with returning to this file
-*/
 void microkit_cml_clear() {
     microkit_dbg_puts("Trying to clear cache\n");
 }
@@ -97,95 +98,49 @@ void microkit_init_pancake_mem() {
     microkit_cml_heap = microkit_cml_memory;
     microkit_cml_stack = microkit_cml_heap + microkit_cml_heap_sz;
     microkit_cml_stackend = microkit_cml_stack + microkit_cml_stack_sz;
+    pnk_mem = (uintptr_t *)microkit_cml_heap;
 }
 
-/* FFI function to check if we have a signal to send */
 void ffimicrokit_have_signal(unsigned char *c, long clen, unsigned char *a, long alen) {
-    uintptr_t *pnk_mem = (uintptr_t *)microkit_cml_heap;
     pnk_mem[clen] = microkit_have_signal;
 }
 
-/* FFI function for seL4_Recv - writes tag and badge to memory */
 void ffimicrokit_recv(unsigned char *c, long clen, unsigned char *a, long alen) {
     seL4_Word badge;
-    seL4_MessageInfo_t tag = seL4_Recv(INPUT_CAP, &badge, REPLY_CAP);
-    
-    uintptr_t *pnk_mem = (uintptr_t *)microkit_cml_heap;
-    
+    microkit_tag = seL4_Recv(INPUT_CAP, &badge, REPLY_CAP);
     pnk_mem[clen] = badge;
-    pnk_mem[alen] = *(seL4_Word*)&tag;  // Just cast it directly
 }
 
-/* FFI function for seL4_ReplyRecv - writes tag and badge to memory */
 void ffimicrokit_reply_recv(unsigned char *c, long clen, unsigned char *a, long alen) {
     seL4_Word badge;
-    uintptr_t *pnk_mem = (uintptr_t *)microkit_cml_heap;
-    
-    // clen = reply_tag_addr, a = badge_addr, alen = tag_addr
-    // Read reply_tag from memory
-    seL4_MessageInfo_t reply_tag = *(seL4_MessageInfo_t*)&pnk_mem[clen];
-    seL4_MessageInfo_t tag = seL4_ReplyRecv(INPUT_CAP, reply_tag, &badge, REPLY_CAP);
-    
-    // Write results to memory
-    pnk_mem[(uintptr_t)a] = badge;     // badge to BADGE_ADDR (500)
-    pnk_mem[alen] = *(seL4_Word*)&tag; // tag to TAG_ADDR (501)
+    microkit_tag = seL4_ReplyRecv(INPUT_CAP, microkit_reply_tag, &badge, REPLY_CAP);
+    pnk_mem[clen] = badge;
 }
 
-/* FFI function for seL4_NBSendRecv - writes tag and badge to memory */
 void ffimicrokit_nb_send_recv(unsigned char *c, long clen, unsigned char *a, long alen) {
     seL4_Word badge;
-    seL4_MessageInfo_t tag = seL4_NBSendRecv(microkit_signal_cap, microkit_signal_msg, INPUT_CAP, &badge, REPLY_CAP);
-    
-    uintptr_t *pnk_mem = (uintptr_t *)microkit_cml_heap;
-    
+    microkit_tag = seL4_NBSendRecv(microkit_signal_cap, microkit_signal_msg, INPUT_CAP, &badge, REPLY_CAP);
     pnk_mem[clen] = badge;
-    pnk_mem[alen] = *(seL4_Word*)&tag;
-    
-    // Clear the signal flag
     microkit_have_signal = seL4_False;
 }
 
-/* FFI function to call fault handler - writes reply info to memory */
 void ffimicrokit_fault_handler(unsigned char *c, long clen, unsigned char *a, long alen) {
-    uintptr_t *pnk_mem = (uintptr_t *)microkit_cml_heap;
-    
-    // c = reply_tag_addr (504), clen = child, a = should_reply_addr (503), alen = tag_addr (501)
-    microkit_child child = clen;
-    seL4_MessageInfo_t msginfo = *(seL4_MessageInfo_t*)&pnk_mem[alen];
-    seL4_MessageInfo_t reply_tag;
-    
-    seL4_Bool should_reply = fault(child, msginfo, &reply_tag);
-    
-    pnk_mem[(uintptr_t)c] = *(seL4_Word*)&reply_tag;
-    pnk_mem[(uintptr_t)a] = should_reply;
+    pnk_mem[alen] = fault(clen, microkit_tag, &microkit_reply_tag);
 }
 
-/* FFI function to call protected handler - writes reply tag to memory */
 void ffimicrokit_protected_handler(unsigned char *c, long clen, unsigned char *a, long alen) {
-    uintptr_t *pnk_mem = (uintptr_t *)microkit_cml_heap;
-    
-    // c = reply_tag_addr (504), clen = channel, a = unused (0), alen = tag_addr (501)
-    microkit_channel channel = clen;
-    seL4_MessageInfo_t msginfo = *(seL4_MessageInfo_t*)&pnk_mem[alen];  // Read tag from TAG_ADDR
-    
-    seL4_MessageInfo_t reply_tag = protected(channel, msginfo);
-    
-    pnk_mem[(uintptr_t)c] = *(seL4_Word*)&reply_tag;  // Write reply_tag to REPLY_TAG_ADDR
+    microkit_reply_tag = protected(clen, microkit_tag);
 }
 
-/* FFI function to call notified handler */
 void ffimicrokit_notified_handler(unsigned char *c, long clen, unsigned char *a, long alen) {
     notified(clen);
 }
 
 void main(void)
 {
-    run_init_funcs();
-    init();
-
     microkit_init_pancake_mem();
-        
-    /*
+    run_init_funcs();
+    init();    /*
      * If we are passive, now our initialisation is complete we can
      * signal the monitor to unbind our scheduling context and bind
      * it to our notification object.
@@ -196,8 +151,6 @@ void main(void)
         microkit_signal_msg = seL4_MessageInfo_new(0, 0, 0, 0);
         microkit_signal_cap = MONITOR_EP;
     }
-
     microkit_cml_main();
-
     handler_loop();
 }
